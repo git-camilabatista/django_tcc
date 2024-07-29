49cb7a43882b2d3474caaa5ce09dbca19fb0583f
Camila <bbatistacamila@gmail.com>
2024-07-29 20:47:19 -0300

    :recycle: Update endpoints

--- django_tcc/setup/web/urls.py
+++ django_tcc/setup/web/urls.py
@@ -1,22 +1,18 @@
 from django.urls import path
-# from .views import UsersView, PurchasesView, PaymentsView, PaidPurchasesView
-
 from .views import (
     register_user, get_user,
-    register_purchase, get_purchase, get_all_purchases,
-    register_payment, get_payment, get_all_payments,
+    register_get_purchase, get_purchase,
+    register_get_payment, get_payment,
     get_all_users, get_paid_purchases
 )
 
 urlpatterns = [
-    path('users/', register_user, name='register_user'),
-    path('users/<int:user_id>/', get_user, name='get_user'),
-    path('purchases/', register_purchase, name='register_purchase'),
-    path('purchases/<int:purchase_id>/', get_purchase, name='get_purchase'),
-    path('all-purchases/', get_all_purchases, name='get_all_purchases'),
-    path('payments/', register_payment, name='register_payment'),
-    path('payments/<int:payment_id>/', get_payment, name='get_payment'),
-    path('all-payments/', get_all_payments, name='get_all_payments'),
-    path('admin/users/', get_all_users, name='get_all_users'),
-    path('admin/paid_purchases/', get_paid_purchases, name='get_paid_purchases'),
+    path('users', register_user, name='register_user'),
+    path('users/<int:user_id>', get_user, name='get_user'),
+    path('purchases', register_get_purchase, name='register_get_purchase'),
+    path('purchases/<int:purchase_id>', get_purchase, name='get_purchase'),
+    path('payments', register_get_payment, name='register_get_payment'),
+    path('payments/<int:payment_id>', get_payment, name='get_payment'),
+    path('admin/users', get_all_users, name='get_all_users'),
+    path('admin/paid_purchases', get_paid_purchases, name='get_paid_purchases'),
 ]
--- django_tcc/setup/web/views.py
+++ django_tcc/setup/web/views.py
@@ -43,17 +43,28 @@ def get_user(request, user_id):
 
 
 # Purchases
-@api_view(['POST'])
-def register_purchase(request):
-    serializer = PurchaseSerializer(data=request.data)
-    if serializer.is_valid():
-        purchase_data = serializer.validated_data
-        if purchase_data['user_id'] not in users:
-            return Response({'detail': 'Invalid or missing user_id'}, status=status.HTTP_400_BAD_REQUEST)
-        purchase_id = len(purchases) + 1
-        purchases[purchase_id] = purchase_data
-        return Response(PurchaseResponseSerializer({'purchase_id': purchase_id, 'message': 'Purchase registered successfully', **purchase_data}).data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['POST', 'GET'])
+def register_get_purchase(request):
+    if request.method == 'POST':
+        serializer = PurchaseSerializer(data=request.data)
+        if serializer.is_valid():
+            purchase_data = serializer.validated_data
+            if purchase_data['user_id'] not in users:
+                return Response({'detail': 'Invalid or missing user_id'}, status=status.HTTP_400_BAD_REQUEST)
+            purchase_id = len(purchases) + 1
+            purchases[purchase_id] = purchase_data
+            return Response(PurchaseResponseSerializer({'purchase_id': purchase_id, 'message': 'Purchase registered successfully', **purchase_data}).data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'GET':
+        user_id = request.headers.get('X-User-Id')
+        user = get_valid_user(int(user_id))
+        if not user:
+            return Response({'detail': 'Invalid user'}, status=status.HTTP_404_NOT_FOUND)
+        user_purchases = {pid: p for pid, p in purchases.items() if p['user_id'] == int(user_id)}
+        if user_purchases:
+            return Response(user_purchases)
+        return Response({'detail': 'No purchases found'}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['GET'])
@@ -68,46 +79,44 @@ def get_purchase(request, purchase_id):
     return Response({'detail': 'Purchase not found'}, status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['GET'])
-def get_all_purchases(request):
-    user_id = request.headers.get('X-User-Id')
-    user = get_valid_user(int(user_id))
-    if not user:
-        return Response({'detail': 'Invalid user'}, status=status.HTTP_404_NOT_FOUND)
-    user_purchases = {pid: p for pid, p in purchases.items() if p['user_id'] == int(user_id)}
-    if user_purchases:
-        return Response(user_purchases)
-    return Response({'detail': 'No purchases found'}, status=status.HTTP_404_NOT_FOUND)
-
-
 # Payments
-@api_view(['POST'])
-def register_payment(request):
-    serializer = PaymentSerializer(data=request.data)
-    if serializer.is_valid():
-        payment_data = serializer.validated_data
-        purchase_id = payment_data['purchase_id']
-        
-        # Verificar se o purchase_id é válido
-        if purchase_id not in purchases:
-            return Response({'detail': 'Invalid or missing purchase_id'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # Verificar se já existe um pagamento registrado para essa compra
-        if any(p['purchase_id'] == purchase_id for p in payments.values()):
-            return Response({'detail': 'Payment already registered for this purchase'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # Registrar o novo pagamento
-        payment_id = len(payments) + 1
-        payments[payment_id] = payment_data
-        purchases[purchase_id]['paid'] = True
-        
-        return Response(PaymentResponseSerializer({
-            'payment_id': payment_id,
-            'message': 'Payment registered successfully',
-            **payment_data
-        }).data)
+@api_view(['POST', 'GET'])
+def register_get_payment(request):
+    if request.method == 'POST':
+        serializer = PaymentSerializer(data=request.data)
+        if serializer.is_valid():
+            payment_data = serializer.validated_data
+            purchase_id = payment_data['purchase_id']
+            
+            # Verificar se o purchase_id é válido
+            if purchase_id not in purchases:
+                return Response({'detail': 'Invalid or missing purchase_id'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # Verificar se já existe um pagamento registrado para essa compra
+            if any(p['purchase_id'] == purchase_id for p in payments.values()):
+                return Response({'detail': 'Payment already registered for this purchase'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # Registrar o novo pagamento
+            payment_id = len(payments) + 1
+            payments[payment_id] = payment_data
+            purchases[purchase_id]['paid'] = True
+            
+            return Response(PaymentResponseSerializer({
+                'payment_id': payment_id,
+                'message': 'Payment registered successfully',
+                **payment_data
+            }).data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        user_id = request.headers.get('X-User-Id')
+        user = get_valid_user(int(user_id))
+        if not user:
+            return Response({'detail': 'Invalid user'}, status=status.HTTP_404_NOT_FOUND)
+        user_payments = {pid: p for pid, p in payments.items() if p['user_id'] == int(user_id)}
+        if user_payments:
+            return Response(user_payments)
+        return Response({'detail': 'No payments found'}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['GET'])
@@ -120,18 +129,6 @@ def get_payment(request, payment_id):
     if payment and payment['user_id'] == int(user_id):
         return Response(payment)
     return Response({'detail': 'Payment not found'}, status=status.HTTP_404_NOT_FOUND)
-
-
-@api_view(['GET'])
-def get_all_payments(request):
-    user_id = request.headers.get('X-User-Id')
-    user = get_valid_user(int(user_id))
-    if not user:
-        return Response({'detail': 'Invalid user'}, status=status.HTTP_404_NOT_FOUND)
-    user_payments = {pid: p for pid, p in payments.items() if p['user_id'] == int(user_id)}
-    if user_payments:
-        return Response(user_payments)
-    return Response({'detail': 'No payments found'}, status=status.HTTP_404_NOT_FOUND)
 
 
 # Admin
